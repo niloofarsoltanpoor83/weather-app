85e067c65ca9059f05300826b98c856ee8df6e64
niloofarsoltanpoor83 <niloofarsoltanpoor83@gmail.com>
2026-07-12 23:06:31 +0330

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function getWeather(){
     if(city === "") return;
 
     const url =
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
     const response = await fetch(url);
 
