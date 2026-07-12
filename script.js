1a25ad3ab2680090ef55951cef0c7e9bd23fe06c
niloofarsoltanpoor83 <niloofarsoltanpoor83@gmail.com>
2026-07-12 22:47:13 +0330

    connect app to weather API

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.getElementById("searchBtn").addEventListener("click", getWeather);
 
+async function getWeather(){
+
+    const city = document.getElementById("cityInput").value;
+
+    if(city === "") return;
+
+    const url =
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    console.log(data);
+
+}
