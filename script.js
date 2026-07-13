4b5280b7145d4ba4a63e2751fadff220732d9070
niloofarsoltanpoor83 <niloofarsoltanpoor83@gmail.com>
2026-07-13 23:59:47 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-console.log("Button clicked");
-console.log("Script Loaded");
-    document.getElementById("searchBtn").addEventListener("click", getWeather);
+const API_KEY = "";
+
+
+const button = document.getElementById("searchBtn");
+
+
+button.addEventListener("click", getWeather);
+
+
 
 async function getWeather(){
 
     const city = document.getElementById("cityInput").value;
 
-    if(city === "") return;
 
-    const url =
+    const url = 
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
-   try {
 
     const response = await fetch(url);
 
-    console.log(response);
 
     const data = await response.json();
 
+
     console.log(data);
 
-} catch(error){
 
-    console.error(error);
+    document.getElementById("weatherResult").innerHTML = `
 
-}
+        <h2>${data.name}</h2>
+
+        <p>${data.main.temp} °C</p>
+
+        <p>${data.weather[0].description}</p>
+
+    `;
+
 }
