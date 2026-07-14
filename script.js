97901bbf3f2c002a45fdff497687f1f1f73be9d0
niloofarsoltanpoor83 <niloofarsoltanpoor83@gmail.com>
2026-07-15 00:11:58 +0330

    add error handling for invalid city names

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
-const API_KEY = "c7246f4ed11c174296162dff32c97872";
-
-
-const button = document.getElementById("searchBtn");
-
-
-button.addEventListener("click", getWeather);
-
-
-
-async function getWeather(){
+async function getWeather() {
 
     const city = document.getElementById("cityInput").value;
 
-
-    const url = 
+    const url =
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-
 
     const response = await fetch(url);
 
-
     const data = await response.json();
 
+    if (data.cod == "404") {
 
-    console.log(data);
+        document.getElementById("weatherResult").innerHTML =
+        "<h2>City not found!</h2>";
 
+        return;
+    }
 
     document.getElementById("weatherResult").innerHTML = `
-
         <h2>${data.name}</h2>
-
         <p>${data.main.temp} °C</p>
-
         <p>${data.weather[0].description}</p>
-
     `;
 
 }
