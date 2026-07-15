fc4b408b7994b17a94c1732b42296b4bfd062a03
niloofarsoltanpoor83 <niloofarsoltanpoor83@gmail.com>
2026-07-15 23:07:49 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const API_KEY = "c7246f4ed11c174296162dff32c97872";
 
 document.getElementById("searchBtn").addEventListener("click", getWeather);
+
 document.getElementById("cityInput").addEventListener("keydown", function(event) {
+
     console.log("Key:", event.key);
 
     if (event.key === "Enter") {
@@ -9,49 +11,84 @@ document.getElementById("cityInput").addEventListener("keydown", function(event)
         console.log("Enter detected");
         getWeather();
     }
+
 });
-console.log("Buttonclicked");
+
+
+console.log("Button clicked");
+
+
 async function getWeather() {
 
-const city = document.getElementById("cityInput").value;
+    try {
 
-document.getElementById("weatherResult").innerHTML =
-"<h3>Loading...</h3>";
-    const url =
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-
-    const response = await fetch(url);
-console.log(response);
-    const data = await response.json();
-const icon = data.weather[0].icon;
-const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-console.log(data);
-    if (data.cod == "404") {
+        const city = document.getElementById("cityInput").value;
 
         document.getElementById("weatherResult").innerHTML =
-        "<h2>City not found!</h2>";
+        "<h3>Loading...</h3>";
 
-        return;
+
+        const url =
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+
+        const response = await fetch(url);
+
+        console.log(response);
+
+
+        const data = await response.json();
+
+        console.log(data);
+
+
+        if (data.cod == "404") {
+
+            document.getElementById("weatherResult").innerHTML =
+            "<h2>City not found!</h2>";
+
+            return;
+        }
+
+
+        const icon = data.weather[0].icon;
+
+        const iconUrl =
+        `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
+
+
+        document.getElementById("weatherResult").innerHTML = `
+
+        <h2>${data.name}</h2>
+
+        <img src="${iconUrl}" alt="Weather Icon">
+
+
+        <p><strong>Temperature:</strong> ${data.main.temp} °C</p>
+
+        <p><strong>Weather:</strong> ${data.weather[0].description}</p>
+
+        <p><strong>Feels Like:</strong> ${data.main.feels_like} °C</p>
+
+        <p><strong>Humidity:</strong> ${data.main.humidity}%</p>
+
+        <p><strong>Wind Speed:</strong> ${data.wind.speed} m/s</p>
+
+        <p><strong>Min Temp:</strong> ${data.main.temp_min} °C</p>
+
+        <p><strong>Max Temp:</strong> ${data.main.temp_max} °C</p>
+
+        `;
+
+
+    } catch(error) {
+
+        console.log(error);
+
+        document.getElementById("weatherResult").innerHTML =
+        "<h2>Error loading weather</h2>";
+
     }
-
-     document.getElementById("weatherResult").innerHTML = `
-
-    <h2>${data.name}</h2>
-
-    <p><strong>Temperature:</strong> ${data.main.temp} °C</p>
-
-    <p><strong>Weather:</strong> ${data.weather[0].description}</p>
-
-    <p><strong>Feels Like:</strong> ${data.main.feels_like} °C</p>
-
-    <p><strong>Humidity:</strong> ${data.main.humidity}%</p>
-
-    <p><strong>Wind Speed:</strong> ${data.wind.speed} m/s</p>
-
-    <p><strong>Min Temp:</strong> ${data.main.temp_min} °C</p>
-
-    <p><strong>Max Temp:</strong> ${data.main.temp_max} °C</p>
-
-`;
 
 }
