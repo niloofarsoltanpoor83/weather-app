3f11cfb9c54200dd34ae84d7e41b6ff87d914e1f
niloofarsoltanpoor83 <niloofarsoltanpoor83@gmail.com>
2026-08-04 22:31:48 +0330

    add dark mode

--- script.js
+++ script.js
@@ -670,4 +670,23 @@ function displayHourlyForecast(data){
 
     });
 
+const themeBtn = document.getElementById("themeBtn");
+
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark");
+    themeBtn.textContent = "☀️ Light Mode";
 }
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem("theme","dark");
+        themeBtn.textContent = "☀️ Light Mode";
+    }else{
+        localStorage.setItem("theme","light");
+        themeBtn.textContent = "🌙 Dark Mode";
+    }
+
+});
